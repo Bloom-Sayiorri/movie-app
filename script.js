5bd76f4999e86da220859f581d5ff45ce0cada29
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-12-16 22:21:22 +0300

    file structure and dependencies

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
+const dotenv = require('dotenv')
+
 const searchBtn = document.querySelector(".search-btn")
 const homeBtn = document.querySelector(".home")
 const genre = document.querySelector(".genre")
 const country = document.querySelector(".country")
 const about = document.querySelector(".about")
 
-searchBtn.addEventListener("click", () => {
-    console.log("Searching for movie")
-})
+dotenv.config()
+const url = process.env.API_KEY
+const apiKey = process.env.API_ACCESS_TOKEN
 
-fetch("https://api.themoviedb.org")
-.then(res => res.json)
-.then(data => console.log(data))
+// Get all Movies
+async function getMovies() {
+    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+    const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`
+    }
+    };
+
+    try {
+        const response = await fetch(url)
+        if(!response.ok) {
+            throw new Error("Network response was not ok.")
+        }
+        return await response.json()
+    } catch (error) {
+        return null
+    }
+}
+
+// Search for  a Movie using id
+function searchMovie() {
+    searchBtn.addEventListener("click", () => {
+        console.log("Searching for movie")
+    })
+}
